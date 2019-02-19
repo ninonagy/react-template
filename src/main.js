48f931cae46222e979ffb9181ff2586540039265
Nino Nađ <41818057+ninonagy@users.noreply.github.com>
2019-02-19 17:33:21 +0100

    Added component example.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import htm from './htm.js';
 const html = htm.bind(createElement);
 
+import ButtonToggle from "./components/Component.js";
+
 const App = () => {
   return(html`
-    <h1>Pure React.js project without toolchain.</h1>
+    <div>
+      <h1>Pure React.js project without toolchain.</h1>
+      <${ButtonToggle} 
+        children=${html`
+          <div>
+            Avoid transpilers and toolchains like Babel and Webpack. 
+            Instead of JSX you use ${' '}
+            <a href="https://github.com/developit/htm">htm</a>.
+          </div>
+        `}
+      />
+    </div>
   `);
 }
 
